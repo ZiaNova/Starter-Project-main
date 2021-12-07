47abbfb635ea7725b6fbc91b11c9e5618f19a3ff
samson1689 <91167069+samson1689@users.noreply.github.com>
2021-12-07 01:31:21 -0500

    Co-authored-by: C.A.Bailey <ZiaNova@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,15 +18,163 @@ $(document).ready(function () {
 const data = [
 	{
 		name: "Boston Common",
-		activity: "meditate",
+		activity: "chill",
 		density: "high",
-		style: "older"
+		style: "old",
+		type:"popular",
+		sound:"high",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/chill/bostoncommons.png'
 	},
 	{
 		name: "Amory Park",
-		activity: "meditate",
+		activity: "chill",
 		density: "medium",
-		style: "newer"
+		style: "new",
+		type: "uncommon",
+		sound: "low",
+		setting: "outdoors",
+		image: 'images/Quietscape-Assests/chill/amorypark.jpg'
+	},
+	{
+		name: "Boston Harbor",
+		activity: "chill",
+		density: "medium",
+		style: "new",
+		type:"popular",
+		sound:"high",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/chill/bostonharbor.jpg'
+	},
+	{
+		name: "BU Beach",
+		activity: "chill",
+		density: "medium",
+		style: "new",
+		type:"popular",
+		sound:"medium",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/chill/bubeach.jpg'
+	},
+	{
+		name: "Fitrec",
+		activity: "workout",
+		density: "high",
+		style: "new",
+		type:"popular",
+		sound:"high",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/workout/fitrec.jpg'
+	},
+	{
+		name: "Gymit",
+		activity: "workout",
+		density: "medium",
+		style: "new",
+		type:"uncommon",
+		sound:"low",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/workout/gymit.jpg'
+	},
+	{
+		name: "BSC",
+		activity: "workout",
+		density: "medium",
+		style: "old",
+		type:"popular",
+		sound:"medium",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/workout/bsc.jpg'
+	},
+	{
+		name: "Esplanade Outdoors Gym",
+		activity: "workout",
+		density: "low",
+		style: "old",
+		type:"uncommon",
+		sound:"low",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/workout/esplanadegym.jpg'
+	},
+	{
+		name: "Charles River Esplanade",
+		activity: "stroll",
+		density: "medium",
+		style: "old",
+		type:"popular",
+		sound:"low",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/stroll/charlesriver.jpg'
+	},
+	{
+		name: "Newbury St",
+		activity: "stroll",
+		density: "high",
+		style: "old",
+		type:"popular",
+		sound:"high",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/stroll/newburyst.jpg'
+	},
+	{
+		name: "Emrald Neckglace Trail",
+		activity: "stroll",
+		density: "low",
+		style: "old",
+		type:"uncommon",
+		sound:"low",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/stroll/emraldneckglace.jpg'
+	},
+	{
+		name: "The Freedom Trail",
+		activity: "stroll",
+		density: "medium",
+		style: "old",
+		type:"popular",
+		sound:"medium",
+		setting:"outdoors",
+		image: 'images/Quietscape-Assests/stroll/freedomtrail.jpg'
+	},
+	{
+		name: "BU Law Library",
+		activity: "study",
+		density: "high",
+		style: "old",
+		type:"popular",
+		sound:"low",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/study/lawlibrary.jpg'
+	},
+	{
+		name: "BU Mugar Library",
+		activity: "study",
+		density: "medium",
+		style: "old",
+		type:"popular",
+		sound:"low",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/study/mugarlibrary.jpg'
+	},
+	{
+		name: "Cafe Nero",
+		activity: "study",
+		density: "medium",
+		style: "new",
+		type:"uncommon",
+		sound:"medium",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/study/cafenero.jpg'
+	},
+	{
+		name: "Thinking Cup",
+		activity: "study",
+		density: "low",
+		style: "old",
+		type:"uncommon",
+		sound:"medium",
+		setting:"indoors",
+		image: 'images/Quietscape-Assests/study/thinkingcup.jpg'
 	}
 ];
 
@@ -34,7 +182,12 @@ const data = [
 let userAnswers = {
 	activity: "",
 	density: "",
-	style: ""
+	style: "",
+	type: "",
+	sound: "",
+	setting: "",
 };
 
+
 function getAnswers() {}
+
